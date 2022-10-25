c0b5aeb685dd58f2a4cb198c6e59c3bcf23feb5c
Xuan Dao <daotx@flextech.vn>
2022-10-25 18:34:02 +0700

    aaaa

--- app/src/main/java/dhcnhn/aduc8386/nixflet/MainActivity.java
+++ app/src/main/java/dhcnhn/aduc8386/nixflet/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.print("d");
         System.out.print("d");
+        System.out.print("d");
+        System.out.print("d");
     }
 }
