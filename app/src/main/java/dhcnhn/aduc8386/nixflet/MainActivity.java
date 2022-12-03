2e996f4c47620e45aac0374f9a3b09aeb04ae46c
Xuan Dao <daotx@flextech.vn>
2022-12-03 21:40:36 +0700

    ui/config

--- app/src/main/java/dhcnhn/aduc8386/nixflet/MainActivity.java
+++ app/src/main/java/dhcnhn/aduc8386/nixflet/MainActivity.java
@@ -3,13 +3,17 @@ package dhcnhn.aduc8386.nixflet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.print("a");
+
     }
+
+
 }
