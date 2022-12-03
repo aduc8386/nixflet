58aad54365dfd15b781b0e68eff7eafa471d27bc
unknown <thuhuongktpm1@gmail.com>
2022-12-03 22:43:10 +0700

    feature/base

---
+++ app/src/main/java/dhcnhn/aduc8386/nixflet/MovieDetailActivity.java
@@ -0,0 +1,18 @@
+package dhcnhn.aduc8386.nixflet;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+
+    }
+
+
+}
---
+++ app/src/main/java/dhcnhn/aduc8386/nixflet/StartingActivity.java
@@ -0,0 +1,36 @@
+package dhcnhn.aduc8386.nixflet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartingActivity extends AppCompatActivity {
+
+    private Button buttonLogin;
+    private Button buttonRegister;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting);
+
+        bindView();
+    }
+
+    private void bindView() {
+        buttonLogin = findViewById(R.id.button_starting_login_button);
+        buttonRegister = findViewById(R.id.button_starting_register_button);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartingActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
